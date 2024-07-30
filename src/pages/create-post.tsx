9a7ad75c2d79eca1026acdb987209ceed0280a84
nghgm <nghgm@nghgm.com>
2024-07-31 01:30:55 +1000

    Version 1.6

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,44 +1,42 @@
 // import { useState } from "react";
 // import { useSession } from "next-auth/react";
 
-// export default function CreatePost() {
-//   const { data: session, status } = useSession();
-//   if (status === "loading") return <p>Loading...</p>;
-//   if (!session) return <p>You must be signed in to view this page</p>;
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
-
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     if (!session) return;
-//     await fetch("/api/posts", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ title, content, authorId: session.user.id }),
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Create a New Post</h1>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Title"
-//           value={title}
-//           onChange={(e) => setTitle(e.target.value)}
-//           required
-//         />
-//         <textarea
-//           placeholder="Content"
-//           value={content}
-//           onChange={(e) => setContent(e.target.value)}
-//           required
-//         />
-//         <button type="submit">Create Post</button>
-//       </form>
-//     </div>
-//   );
-// }
+export default function CreatePost() {
+  //   const { data: session, status } = useSession();
+  //   if (status === "loading") return <p>Loading...</p>;
+  //   if (!session) return <p>You must be signed in to view this page</p>;
+  //   const [title, setTitle] = useState("");
+  //   const [content, setContent] = useState("");
+  //   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //     e.preventDefault();
+  //     if (!session) return;
+  //     await fetch("/api/posts", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ title, content, authorId: session.user.id }),
+  //     });
+  //   };
+  //   return (
+  //     <div>
+  //       <h1>Create a New Post</h1>
+  //       <form onSubmit={handleSubmit}>
+  //         <input
+  //           type="text"
+  //           placeholder="Title"
+  //           value={title}
+  //           onChange={(e) => setTitle(e.target.value)}
+  //           required
+  //         />
+  //         <textarea
+  //           placeholder="Content"
+  //           value={content}
+  //           onChange={(e) => setContent(e.target.value)}
+  //           required
+  //         />
+  //         <button type="submit">Create Post</button>
+  //       </form>
+  //     </div>
+  //   );
+}
